0cf5cd923644e2b6cb21cd8a8d31a73cc6420542
AkashSDas <57318290+AkashSDas@users.noreply.github.com>
2021-02-08 13:56:04 +0530

    Update 18-Getters-and-Setters.dart

--- 18-Getters-and-Setters.dart
+++ 18-Getters-and-Setters.dart
@@ -12,9 +12,38 @@ class Rectangle {
   set buttom(num value) => top = value - height;
 }
 
+class Employee {
+  String first_name;
+  String last_name;
+  int age;
+  double salary_amount;
+
+  Employee(String first_name, last_name, int age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.age = age;
+  }
+
+  void set salary(double salary) {
+    if (salary < 0) {
+      this.salary_amount = 0;
+    } else {
+      this.salary_amount = salary * (30 / 100);
+    }
+  }
+
+  double get salary {
+    return this.salary_amount;
+  }
+}
+
 void main() {
   var rect = Rectangle(3, 4, 20, 15);
   print(rect.left);
   rect.right = 12; // Setting
   print(rect.left);
+  
+  var emp = new Employee('James', 'Bond', 10);
+  emp.salary = 100;
+  print(emp.salary);
 }
